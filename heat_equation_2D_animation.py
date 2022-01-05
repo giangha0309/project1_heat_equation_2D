d0eae3e19b512ff28adb250c77a4028266381daf
Giang <giang.tdk194265@sis.hust.edu.vn>
2022-01-05 20:28:35 +0700

    gc

--- heat_equation_2D_animation.py
+++ heat_equation_2D_animation.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimationgit
 m = n = 21
 dx = dy = 0.1
 D = 0.1
