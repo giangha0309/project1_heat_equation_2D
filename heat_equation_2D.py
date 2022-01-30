f0a03057db794ee92e8e1c0dc1dd174a707938dc
Giang <giang.tdk194265@sis.hust.edu.vn>
2022-01-30 23:13:08 +0700

    commit project 1

--- heat_equation_2D.py
+++ heat_equation_2D.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 """
 11
 11
@@ -24,14 +25,18 @@ Th = int(input())
 Tc = int(input())
 e1 = int(input())  # 5
 e2 = int(input())  # 3
+X
+dx2, dy2 = dx * dx, dy * dy  # Bình phương kích thước điểm lưới theo chiều x, y
+t0 = int(T / dt)  # Tổng số lần lặp
 
-
-dx2, dy2 = dx * dx, dy * dy
 c = Tc * np.ones((m + 2, n + 2))
 
 for i in range(m + 2):
     for j in range(n + 2):
-        if i in range(int(m / 2 - e1 + 1), int(m / 2 + e1 + 1 + bool(m % 2 == 1))) and j in range(int(n / 2 - e2 + 1), int(n / 2 + e2 + 1 + bool(m % 2 == 1))): c[i][j] = Th
+        if i in range(int(m / 2 - e1 + 1), int(m / 2 + e1 + 1 + bool(m % 2 == 1))) and j in range(int(n / 2 - e2 + 1),
+                                                                                                  int(n / 2 + e2 + 1 + bool(
+                                                                                                      m % 2 == 1))):
+            c[i][j] = Th
 
 
 def do_timestep():
@@ -40,20 +45,24 @@ def do_timestep():
             + (c[1:-1, 2:] - 2 * c[1:-1, 1:-1] + c[1:-1, :-2]) / dy2)
 
 
-t0 = int(T / dt)
+fig = plt.figure()
+
 mfig = [0, int(t0 / 8), int(t0 / 4), int(3 * t0 / 8), int(t0 / 2), int(5 * t0 / 8), int(3 * t0 / 4), int(7 * t0 / 8),
         t0]
 fignum = 0
-fig = plt.figure()
 for t in range(t0 + 1):
     if t in mfig:
-        fignum += 1, row, index
+        fignum += 1
         ax = fig.add_subplot(330 + fignum)  # column
         ax.set_axis_off()  # turn off axis
         im = ax.imshow(c.copy(), cmap=plt.get_cmap('YlOrRd'), vmin=Tc, vmax=Th)  # do thi im gan vao khung ax
         ax.set_title('{:.1f} ms'.format(t * dt * 1000))
     do_timestep()
-fig.subplots_adjust(right=0.85)
+fig.subplots_adjust(right=0.85)  #
 cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])  # left, bottom, witdh, height
+fig.colorbar(im, cbar_ax)
 plt.show()
+
+fig.subplots_adjust(right=0.85)
+cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
 fig.colorbar(im, cbar_ax)
--- heat_equation_2D_animation.py
+++ heat_equation_2D_animation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
 """
 11
 11
@@ -29,10 +30,12 @@ e2 = int(input())  # 3
 dx2, dy2 = dx * dx, dy * dy
 c = Tc * np.ones((m + 2, n + 2))
 
-
 for i in range(m + 2):
     for j in range(n + 2):
-        if i in range(int(m / 2 - e1 + 1), int(m / 2 + e1 + 1 + bool(m % 2 == 1))) and j in range(int(n / 2 - e2 + 1), int(n / 2 + e2 + 1 + bool(m % 2 == 1))): c[i][j] = Th
+        if i in range(int(m / 2 - e1 + 1), int(m / 2 + e1 + 1 + bool(m % 2 == 1))) and j in range(int(n / 2 - e2 + 1),
+                                                                                                  int(n / 2 + e2 + 1 + bool(
+                                                                                                      m % 2 == 1))):
+            c[i][j] = Th
 
 
 def do_timestep():
@@ -44,7 +47,7 @@ def do_timestep():
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_axis_off()
-cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])  # left, bottom, witdh, height
+cbar_ax = fig.add_axes([0.9, 0.15, 0.03, 0.7])
 fig.subplots_adjust(right=0.85)
 
 
@@ -55,5 +58,6 @@ def animate(i):
     return im
 
 
-anim = FuncAnimation(fig, animate, np.arange(0, int(T/dt + 1)), interval=10, repeat=0)
+anim = FuncAnimation(fig, animate, np.arange(0, int(T / dt + 1)), interval=10, repeat=0, save_count=200)
+anim.save('test.mp4')
 plt.show()
